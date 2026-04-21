cfc75b8fca5a8c88852f4ac7101a894f01b29aa0
Li Siyuan <1070425210@stu.jiangnan.edu.cn>
2026-04-21 14:18:59 +0800

    fix: use hash routing for github pages preview

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./containers/App/App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+
+const useHashRouter = import.meta.env.VITE_ROUTER_MODE === "hash";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
+    {useHashRouter ? (
+      <HashRouter>
+        <App />
+      </HashRouter>
+    ) : (
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <App />
+      </BrowserRouter>
+    )}
   </React.StrictMode>,
 );
